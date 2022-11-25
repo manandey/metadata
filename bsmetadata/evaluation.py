13ffee75e6487ab1f14084ceaa8768b195b628ad
Manan Dey <manandey1@gmail.com>
2022-11-25 15:48:28 +0530

    Update evaluation.py

--- bsmetadata/evaluation.py
+++ bsmetadata/evaluation.py
@@ -135,12 +135,12 @@ if __name__ == "__main__":
         # "bs-modeling-metadata/c4-en-html-with-validation_metadata_entity",
         "bs-modeling-metadata/c4-en-html-with-validation_metadata_entity_paragraph",
         "bs-modeling-metadata/c4-en-html-with-validation_metadata_website_desc",
-        "bs-modeling-metadata/c4-en-html-with-validation_metadata_generation_datasource",
+        # "bs-modeling-metadata/c4-en-html-with-validation_metadata_generation_datasource",
         "bs-modeling-metadata/c4-en-html-with-validation_metadata_timestamp",
-        "bs-modeling-metadata/c4-en-html-with-validation_metadata_title",
-        "bs-modeling-metadata/c4-en-html-with-validation_metadata_generation_length_sentence",
-        "bs-modeling-metadata/c4-en-html-with-validation_metadata_generation_length_text",
-        "bs-modeling-metadata/c4-en-html-with-validation_metadata_url",
+        # "bs-modeling-metadata/c4-en-html-with-validation_metadata_title",
+        # "bs-modeling-metadata/c4-en-html-with-validation_metadata_generation_length_sentence",
+        # "bs-modeling-metadata/c4-en-html-with-validation_metadata_generation_length_text",
+        # "bs-modeling-metadata/c4-en-html-with-validation_metadata_url",
         # "bs-modeling-metadata/c4-en-html-with-validation_metadata_paragraph",
     ]
 
